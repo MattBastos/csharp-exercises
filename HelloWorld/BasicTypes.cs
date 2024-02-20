1c478905ba640f47d0aaa12c1f2940b4c56042ed
Matthews Bastos <matthewsbasttos@gmail.com>
2024-02-20 13:57:02 -0300

    Creates BasicTypes class and PrintBasicTypes method

---
+++ HelloWorld/BasicTypes.cs
@@ -0,0 +1,16 @@
+namespace HelloWorld
+{
+    class BasicTypes
+    {
+        public static void PrintBasicTypes()
+        {
+            byte firstByte = 126;
+            int firstInt = 1000;
+            long firstLong = 2147483648L;
+
+            Console.WriteLine(firstByte);
+            Console.WriteLine(firstInt);
+            Console.WriteLine(firstLong);
+        }
+    }
+}
--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,10 +1,16 @@
-﻿namespace HelloWorld
+﻿// Create New Project = dotnet new console/web -n NameOfTheProject
+// Run Main = dotnet run
+
+namespace HelloWorld
 {
     class Program
     {
         static void Main()
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("-----------------------");
+
+            BasicTypes.PrintBasicTypes();
         }
     }
 }
